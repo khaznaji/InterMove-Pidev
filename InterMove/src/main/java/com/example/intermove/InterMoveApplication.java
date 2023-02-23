0581ca8e31426a45132f41cfb467538e566e4350
RayenKh99 <rinokhadraoui1@gmail.com>
2023-02-23 11:43:23 +0100

    First commit

--- InterMove/src/main/java/com/example/intermove/InterMoveApplication.java
+++ InterMove/src/main/java/com/example/intermove/InterMoveApplication.java
@@ -1,7 +1,11 @@
 package com.example.intermove;
 
+import com.example.intermove.Entities.Accomodation.House;
+import com.example.intermove.Repositories.Accomodation.IHousesRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
 
 @SpringBootApplication
 public class InterMoveApplication {
